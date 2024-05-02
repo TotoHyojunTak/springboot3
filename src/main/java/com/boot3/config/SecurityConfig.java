cf3b63bead742d536dbf06551603344055114299
Toto Hyojun Tak <thj0309@hotmail.com>
2024-05-02 15:05:02 +0900

    Spring boot 3.2.5 upgrade

--- src/main/java/com/boot3/config/SecurityConfig.java
+++ src/main/java/com/boot3/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,19 +19,20 @@ import org.springframework.security.web.firewall.HttpFirewall;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);  // CSRF 보호 비활성화
 
-        // HttpSecurity : 인증, 인가 API의 설정을 제공
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http.authorizeHttpRequests()
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)); // X-Frame-Options 비활성화
+        http.authorizeRequests(requests -> requests
                 .requestMatchers("/swagger*/**").permitAll()
                 .requestMatchers("/user/**").permitAll()
                 .anyRequest().permitAll()
-
-                .and()
-                .logout().logoutUrl("/logout")
-                .logoutSuccessUrl("/login").permitAll();
+        );
+        http.logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .permitAll()
+        );
 
         return http.build();
     }
